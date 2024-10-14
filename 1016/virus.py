65346475aac1d2a98ee9e74ad0fbb0cba45630e9
sujinee01 <kmosujin@gmail.com>
2024-10-15 03:27:13 +0900

    Test: solving problem 'virus'

--- 1016/virus.py
+++ 1016/virus.py
@@ -1,6 +1,18 @@
 num = int(input()) #컴퓨터수
 net = int(input()) #네트워크 연결 컴퓨터수
 
-i = list(range(1,num+1))
+# 그래프 초기화
+graph = [[] for _ in range(num + 1)]
+print(graph)
 
-v = map(int,input().split())
+# 간선 정보 입력받기
+for _ in range(net):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(graph)
+    
+# 방문 기록 리스트
+visited = [False] * (num + 1)
+print(visited)
