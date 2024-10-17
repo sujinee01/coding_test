ca731eca3f008bf47ba9936dd2f30b17d54b5738
sujinee01 <kmosujin@gmail.com>
2024-10-17 15:01:30 +0900

    Fix: saparate cases hour and minute

--- 1017/alarm.py
+++ 1017/alarm.py
@@ -1,6 +1,12 @@
 h, m = map(int, input().split());
 
-if m >= 45:
-    print(h,(m-45));
+if h>0:
+    if m >= 45:
+        print(h,(m-45));
+    else:
+        print((h-1),(m+60-45));
 else:
-    print((h-1),(m+60-45));
+    if m >= 45:
+        print(h,(m-45));
+    else:
+        print((h:=23),(m+60-45));
