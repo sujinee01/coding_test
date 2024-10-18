889818136c2cdc8e6cc0c2472022b78b82df96d8
sujinee01 <kmosujin@gmail.com>
2024-10-18 13:43:59 +0900

    Fix: Final Code  'Automatic Time Calculation System'

--- 1018/Automatic_Time_Calculation_System.py
+++ 1018/Automatic_Time_Calculation_System.py
@@ -9,7 +9,4 @@ m += time;
 if m < 60:
     print(h, m);
 else:
-    if h+int(m/60) < 24:
-        print(h+int(m/60), m-int(60*int(m/60)));
-    else:
-        print(0, m-int(60*int(m/60)));
+    print(h+int(m/60)-(int((h+int(m/60))/24)*24), m-int(60*int(m/60)));
