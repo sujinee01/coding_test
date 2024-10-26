bee3354fe5f78c4147318afea68e8b6b000f01df
sujinee01 <kmosujin@gmail.com>
2024-10-27 02:55:52 +0900

    modify 'Mathematical_Constant'

--- 1027/Mathematical_Constant .py
+++ 1027/Mathematical_Constant .py
@@ -1,9 +1,8 @@
-# 두 숫자를 입력받습니다.
+# Mathematical_Constant
 A, B = input().split()
 
-# 각 숫자를 뒤집고 정수로 변환합니다.
+# 각 숫자를 뒤집고 정수로 변환
 reversed_A = int(A[::-1])
 reversed_B = int(B[::-1])
 
-# 두 숫자 중 큰 값을 출력합니다.
 print(max(reversed_A, reversed_B))
